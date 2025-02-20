098bf548bb8bfddf5bb2a5eee9bb8b7aefd17513
Smruti-Rekha-Swain <smrutirekhaswain.19@gmail.com>
2025-02-20 20:21:41 +0530

    Added functions and beautified UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,93 +1,148 @@
+import { useState } from "react";
+
 function App() {
+  const [output, setOutput] = useState(0);
+  const operand = [];
+  const operator = [];
+  function number(inp) {}
+  function oprt() {}
+  function special() {}
+  function clear() {}
+  function backspace() {}
+  function dec() {}
+  function calculate() {}
+
   return (
     <>
-      <div className="container-fluid text-center d-flex flex-column w-25 align-items-center">
-        <div className="row ">hii</div>
+      <div className="container flex-column w-50 d-flex align-items-center">
+        <div className="row ">
+          <div className="col">{output}</div>
+        </div>
 
-        <div className="row d-flex flex-column">
-          <div className="btn-group col" role="group">
-            <button type="button" className="btn btn-primary">
-              AC
+        <div className="row p-2">
+          <div className="col-10  gap-1 justify-content-around d-flex flex-wrap">
+            <button className="btn btn-outline-warning" onClick={clear}>
+              C
             </button>
-            <button type="button" className="btn btn-primary">
+            <button className="btn btn-outline-warning" onClick={backspace}>
               B
             </button>
-            <button type="button" className="btn btn-primary">
+            <button
+              className="btn btn-outline-secondary"
+              onClick={() => oprt(this.value)}
+              value={"%"}
+            >
               %
             </button>
-            <button type="button" className="btn btn-primary">
+            <button
+              className="btn btn-outline-secondary"
+              onClick={() => oprt(this.value)}
+              value={"/"}
+            >
               /
             </button>
-          </div>
-          <div
-            className="btn-group col"
-            role="group"
-            aria-label="Basic example"
-          >
-            <button type="button" className="btn btn-primary">
+
+            <button
+              className="btn btn-outline-primary"
+              onClick={() => number(this.value)}
+              value={7}
+            >
               7
             </button>
-            <button type="button" className="btn btn-primary">
+            <button
+              className="btn btn-outline-primary"
+              onClick={() => number(this.value)}
+              value={8}
+            >
               8
             </button>
-            <button type="button" className="btn btn-primary">
+            <button
+              className="btn btn-outline-primary"
+              onClick={() => number(this.value)}
+              value={9}
+            >
               9
             </button>
-            <button type="button" className="btn btn-primary">
+            <button
+              className="btn btn-outline-secondary"
+              onClick={() => oprt(this.value)}
+              value={"*"}
+            >
               *
             </button>
-          </div>
-          <div
-            className="btn-group col"
-            role="group"
-            aria-label="Basic example"
-          >
-            <button type="button" className="btn btn-primary">
+
+            <button
+              className="btn btn-outline-primary"
+              onClick={() => number(this.value)}
+              value={4}
+            >
               4
             </button>
-            <button type="button" className="btn btn-primary">
+            <button
+              className="btn btn-outline-primary"
+              onClick={() => number(this.value)}
+              value={5}
+            >
               5
             </button>
-            <button type="button" className="btn btn-primary">
+            <button
+              className="btn btn-outline-primary"
+              onClick={() => number(this.value)}
+              value={6}
+            >
               6
             </button>
-            <button type="button" className="btn btn-primary">
+            <button
+              className="btn btn-outline-secondary"
+              onClick={() => oprt(this.value)}
+              value={"-"}
+            >
               -
             </button>
-          </div>
-          <div
-            className="btn-group col"
-            role="group"
-            aria-label="Basic example"
-          >
-            <button type="button" className="btn btn-primary">
+
+            <button
+              className="btn btn-outline-primary"
+              onClick={() => number(this.value)}
+              value={1}
+            >
               1
             </button>
-            <button type="button" className="btn btn-primary">
+            <button
+              className="btn btn-outline-primary"
+              onClick={() => number(this.value)}
+              value={2}
+            >
               2
             </button>
-            <button type="button" className="btn btn-primary">
+            <button
+              className="btn btn-outline-primary"
+              onClick={() => number(this.value)}
+              value={3}
+            >
               3
             </button>
-            <button type="button" className="btn btn-primary">
+            <button
+              className="btn btn-outline-secondary"
+              onClick={() => oprt(this.value)}
+              value={"+"}
+            >
               +
             </button>
-          </div>
-          <div
-            className="btn-group col"
-            role="group"
-            aria-label="Basic example"
-          >
-            <button type="button" className="btn btn-primary">
-              00
+
+            <button className="btn btn-outline-warning" onClick={special}>
+              +/-
             </button>
-            <button type="button" className="btn btn-primary">
+            <button
+              className="btn btn-outline-primary"
+              onClick={() => number(this.value)}
+              value={0}
+            >
               0
             </button>
-            <button type="button" className="btn btn-primary">
+            <button className="btn btn-outline-danger" onClick={dec}>
               .
             </button>
-            <button type="button" className="btn btn-primary">
+            <button className="btn btn-outline-success" onClick={calculate}>
               =
             </button>
           </div>
