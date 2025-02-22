093cdd5e9c9bb62e49b8daeb1de712773260d02c
Smruti-Rekha-Swain <smrutirekhaswain.19@gmail.com>
2025-02-22 19:58:45 +0530

    Restructured UI. Implemented logic for decimal button. Implemented the helper function `isDigit()`. Refactored some code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
 
 function App() {
-  const [output, setOutput] = useState(""); //create another state variable with const[operator,setOperater]=useState()
-  const operand = [];
-  const operator = [];
+  const [output, setOutput] = useState("");
 
   function number(inp) {
     if (output == "" && inp == 0) {
@@ -24,151 +22,208 @@ function App() {
   }
 
   function special() {
-    if (output == "") {
-      setOutput(output + "-");
-    }
-    const last = output.toString().charAt(output.length - 1);
-    const lastChar = ["-", "+", "*", "/", "%"].includes(last);
-    if (lastChar && last == "-") {
-      setOutput(output.slice(0, -1) + "+");
-    } else if (lastChar && last == "+") {
-      setOutput(output.slice(0, -1) + "-");
-    } else if (lastChar) {
-      setOutput(output + "-");
-    } else setOutput("-" + output);
+    const last = output.at(-1);
+    if (!isDigit()) {
+      switch (last) {
+        case "-":
+          setOutput(output.slice(0, -1) + "+");
+          break;
+        case "+":
+          setOutput(output.slice(0, -1) + "-");
+          break;
+        default:
+          setOutput(output + "-");
+      }
+    } else {
+      // Place a "-" at the start of the current number and NOT at the start of the output
 
-    console.log(last);
+    }
   }
+
   function clear() {
     setOutput("");
   }
+
   function backspace() {
     if (output === "") return;
     setOutput(output.slice(0, -1));
   }
-  function dec() {}
+
+  function dec() {
+    if (output === "") {
+      setOutput(output + "0.");
+      return;
+    }
+
+    let i = output.length - 1;
+    // This loop will traverse the string backwards to check if the current number is already decimal or not.
+    while (isDigit(output.at(i)) && i >= 0) {
+      i--;
+    }
+    // Found the current number as decimal and do nothing.
+    if (output.at(i) == ".") return;
+    // Check that the last character is an operator and append decimal after a zero.
+    else if (!isDigit()) setOutput(output + "0.");
+    // Here conclude that the current number is integer and we can convert it into decimal.
+    else setOutput(output + ".");
+  }
+
   function calculate() {
     if (output === "") return;
     setOutput(eval(output).toString());
   }
+
+  function isDigit(d = output.at(-1), skipDot = false) {
+    try {
+      return typeof eval(d) == "number";
+    } catch (error) {
+      return false || skipDot;
+    }
+  }
+
   if (output === "0") setOutput("");
 
   return (
     <>
-      <div className="container flex-column w-50 d-flex align-items-center">
-        <div className="row">
-          <div className="col">{output == "" ? 0 : output}</div>
-        </div>
-
-        <div className="row p-2">
-          <div className="col-10  gap-1 justify-content-around d-flex flex-wrap">
-            <button className="btn btn-outline-warning" onClick={clear}>
+      <div className="container d-flex">
+        <div className="col-2 offset-3 w-50 border border-3 px-3 mt-5">
+          <div className="row my-1 border border-2">
+            <div className="col text-end overflow-hidden">
+              {output == "" ? 0 : output}
+            </div>
+          </div>
+          <div className="row justify-content-around gap-3">
+            <button className="btn btn-outline-warning col" onClick={clear}>
               C
             </button>
-            <button className="btn btn-outline-warning" onClick={backspace}>
+            <button className="btn btn-outline-warning col" onClick={backspace}>
               B
             </button>
             <button
-              className="btn btn-outline-secondary"
+              className="btn btn-outline-secondary col"
               onClick={() => oprt("%")}
             >
               %
             </button>
             <button
-              className="btn btn-outline-secondary"
+              className="btn btn-outline-secondary col"
               onClick={() => oprt("/")}
             >
               /
             </button>
-
+          </div>
+          <div className="row justify-content-around gap-3 mt-3">
             <button
-              className="btn btn-outline-primary"
-              onClick={() => number(7)}
+              className="btn btn-outline-primary col"
+              onClick={() => {
+                number(7);
+              }}
             >
               7
             </button>
             <button
-              className="btn btn-outline-primary"
-              onClick={() => number(8)}
+              className="btn btn-outline-primary col"
+              onClick={() => {
+                number(8);
+              }}
             >
               8
             </button>
             <button
-              className="btn btn-outline-primary"
-              onClick={() => number(9)}
+              className="btn btn-outline-primary col"
+              onClick={() => {
+                number(9);
+              }}
             >
               9
             </button>
             <button
-              className="btn btn-outline-secondary"
+              className="btn btn-outline-secondary col"
               onClick={() => oprt("*")}
             >
               *
             </button>
-
+          </div>
+          <div className="row justify-content-around gap-3 mt-3">
             <button
-              className="btn btn-outline-primary"
-              onClick={() => number(4)}
+              className="btn btn-outline-primary col"
+              onClick={() => {
+                number(4);
+              }}
             >
               4
             </button>
             <button
-              className="btn btn-outline-primary"
-              onClick={() => number(5)}
+              className="btn btn-outline-primary col"
+              onClick={() => {
+                number(5);
+              }}
             >
               5
             </button>
             <button
-              className="btn btn-outline-primary"
-              onClick={() => number(6)}
+              className="btn btn-outline-primary col"
+              onClick={() => {
+                number(6);
+              }}
             >
               6
             </button>
             <button
-              className="btn btn-outline-secondary"
+              className="btn btn-outline-secondary col"
               onClick={() => oprt("-")}
             >
               -
             </button>
-
+          </div>
+          <div className="row justify-content-around gap-3 mt-3">
             <button
-              className="btn btn-outline-primary"
-              onClick={() => number(1)}
+              className="btn btn-outline-primary col"
+              onClick={() => {
+                number(1);
+              }}
             >
               1
             </button>
             <button
-              className="btn btn-outline-primary"
-              onClick={() => number(2)}
+              className="btn btn-outline-primary col"
+              onClick={() => {
+                number(2);
+              }}
             >
               2
             </button>
             <button
-              className="btn btn-outline-primary"
-              onClick={() => number(3)}
+              className="btn btn-outline-primary col"
+              onClick={() => {
+                number(3);
+              }}
             >
               3
             </button>
             <button
-              className="btn btn-outline-secondary"
+              className="btn btn-outline-secondary col"
               onClick={() => oprt("+")}
             >
               +
             </button>
-
-            <button className="btn btn-outline-warning" onClick={special}>
+          </div>
+          <div className="row justify-content-around gap-3 mt-3 mb-1">
+            <button className="btn btn-outline-warning col" onClick={special}>
               +/-
             </button>
             <button
-              className="btn btn-outline-primary"
-              onClick={() => number(0)}
+              className="btn btn-outline-primary col"
+              onClick={() => {
+                number(0);
+              }}
             >
               0
             </button>
-            <button className="btn btn-outline-danger" onClick={dec}>
+            <button className="btn btn-outline-danger col" onClick={dec}>
               .
             </button>
-            <button className="btn btn-outline-success" onClick={calculate}>
+            <button className="btn btn-outline-success col" onClick={calculate}>
               =
             </button>
           </div>
